0dd82d2d42dad9e079b8d4eac9aebe5585bb8616
huangyichun <huangyichun@jd.com>
2021-03-24 15:09:12 +0800

    init project

---
+++ src/main/java/com/huang/remotetransfer/RemoteTransferApplication.java
@@ -0,0 +1,13 @@
+package com.huang.remotetransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteTransferApplication.class, args);
+    }
+
+}
